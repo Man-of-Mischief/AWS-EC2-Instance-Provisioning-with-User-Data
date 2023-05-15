9eac29e784c7a6fe06d1379987555c444507a0ea
Nidhin B <106511145+Man-of-Mischief@users.noreply.github.com>
2023-05-15 21:57:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAVRHLD74FRUVGZC4D"
-  secret_key = "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"
+  access_key = "AKIAVRHLabcedefegag"
+  secret_key = "JfCEdXfadjfnsfkn_+-=afsfknkjsnfdErv++81KlXHWLI/"
 }
